35eb39c728cbdbba15a2cfb00ca488fb0ea75b45
Angus <angus.goody@gmail.com>
2021-11-16 00:35:58 +0000

    Adding new navigation bar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }"  class="bg-gray-800 shadow-md">
+{{-- <nav x-data="{ open: false }"  class="bg-gray-800 shadow-md">
   <div class="container mx-auto px-6 md:px-3 ">
     <div class="relative flex items-center justify-between h-16">
       
@@ -93,7 +93,7 @@
               <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
               <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-        </div>
+        </div> 
 
         <div class="mt-3 space-y-1">
           <!--Other links-->
@@ -123,4 +123,40 @@
 
   </div>
   </div>
-</nav>
+</nav> --}}
+
+<div class="navbar mb-2 shadow-lg bg-neutral-focus text-neutral-content">
+  <div class="container mx-auto px-6 md:px-3 ">
+    <div class="px-2 mx-2 navbar-start">
+      <img class="block lg:hidden h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
+      <img class="hidden lg:block h-8 w-auto" src="/assets/images/core/logo.svg" alt="Workflow">
+    </div> 
+    <div class="hidden px-2 mx-2 navbar-center lg:flex">
+      <div class="flex items-stretch space-x-1">
+        <a {!! Request::is('/') ? '' : 'href="/"' !!} class="btn btn-sm rounded-btn {{ Request::is('/') ? 'btn-active bg-gray-500 hover:bg-gray-500' : 'btn-ghost hover:bg-gray-600'}}">
+          Home
+        </a>
+        <a {!! Request::is('ass') ? '' : "href='/ass'" !!} class="btn btn-sm rounded-btn {{ Request::is('ass') ? 'btn-active bg-gray-500 hover:bg-gray-500' : 'btn-ghost hover:bg-gray-600'}}">
+          A$$word
+        </a>
+        <a {!! Request::is('common') ? '' : 'href="/common"' !!} class="btn btn-sm rounded-btn {{ Request::is('common') ? 'btn-active bg-gray-500 hover:bg-gray-500' : ' btn-ghost hover:bg-gray-600'}}">
+          Common
+        </a>
+
+      </div>
+    </div> 
+    <div class="navbar-end text-right">
+      <button class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">     
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>                     
+        </svg>
+      </button> 
+      <button class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">             
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
+        </svg>
+      </button>
+    </div>
+  </div>
+</div>
+
