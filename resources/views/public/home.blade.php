6582c44d29869fd4283914a759e57cd084e0c2f7
Angus <angus.goody@gmail.com>
2021-12-06 23:58:03 +0000

    Refresh button now works

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -32,7 +32,7 @@
               <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
             </svg>
           </button>
-          <button class="btn btn-ghost btn-square">
+          <button class="btn btn-ghost btn-square" id="refreshButton">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
             </svg>
@@ -99,6 +99,9 @@
     const $slider = $("#lengthSlider");
     const $sliderLabel = $("#lengthLabel")
 
+    //Refresh button
+    const $refreshButton = $("#refreshButton");
+
     //Label
     const $passwordLabel=$("#passwordLabel");
     const $lengthLabel = $("#lengthLabel")
@@ -146,6 +149,10 @@
 
     });
 
+    //When refresh is clicked
+    $($refreshButton).click(function(){
+      getSliderAndUpdate();
+    });
 
 
     function getSliderAndUpdate(val=document.getElementById("lengthSlider").value){
