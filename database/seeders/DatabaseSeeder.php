4ae7a173d56ff446a9765bb9083ba5218494cf2f
Angus Goody <angus.goody@gmail.com>
2021-12-12 20:37:48 +0000

    Removed UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
+        //$this->call(UserSeeder::class);
 
         $this->call(WordtypeSeeder::class);
         $this->call(WordSeeder::class);
