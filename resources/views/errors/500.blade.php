db241ffc53391a1743e33ef653a3b964d3df834e
Angus Goody <angus.goody@gmail.com>
2021-12-13 18:49:41 +0000

    Added UserSeeder.php and moved admin credentials to ENV file

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,4 +1,4 @@
-@section('title', 'Not Found')
+@section('title', 'Server Error')
 <x-error-layout>
     <x-error-page code="500" title="Server error" subtitle="Passworld ran into a problem, please try again" />
 </x-error-layout>
