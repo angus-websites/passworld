80072d056f6344f0f9a7b697e8b62c8edeb12f96
Angus <angus.goody@gmail.com>
2022-06-14 21:24:09 +0100

    added buttons for approve and deny

--- app/Http/Livewire/Suggestion/Index.php
+++ app/Http/Livewire/Suggestion/Index.php
@@ -3,11 +3,12 @@
 namespace App\Http\Livewire\Suggestion;
 
 use Livewire\Component;
+use App\Models\Suggestion;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.suggestion.index');
+        return view('livewire.suggestion.index', ["suggestions" => Suggestion::all()]);
     }
 }
