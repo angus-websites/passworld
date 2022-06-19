37b195568b0646d074d47a19ac72b47e04f3c87f
Angus <angus.goody@gmail.com>
2022-06-19 14:52:55 +0100

    ui tweaks

--- resources/views/components/error-page.blade.php
+++ resources/views/components/error-page.blade.php
@@ -14,9 +14,9 @@
           <span class="block xl:inline">{{$title}}</span>
         </h1>
         <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">{{$subtitle}}</p>
-        <div class="mt-5 bg-turtleorange">
-          <a href="{{ url()->previous() }}" class="btn btn-turtleorange">Back</a>
-          <a href="/" class="btn btn-accent">Home</a>
+        <div class="mt-5">
+          <x-link-button href="{{ url()->previous() }}">Back</x-link-button>
+          <x-link-button href="/" class="btn-accent">Home</x-link-button>
         </div>
       </div>
     </div>
--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,6 +1,6 @@
 <x-master-layout>
   @include('includes.navbar-naked')
-  <main class="text-gray-600 body-font overflow-hidden min-h-screen">
+  <main class="body-font overflow-hidden min-h-screen">
     {{ $slot }}
   </main>
   @include('includes.footer')
