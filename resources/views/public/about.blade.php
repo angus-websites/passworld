d9214433bab31cb6940e9a7ea625e207a284b26c
Angus <angus.goody@gmail.com>
2022-06-19 16:30:12 +0100

    updated about page

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -2,22 +2,6 @@
 @section('description', "Learn about passworld")
 <x-app-layout>
   <x-page-container>
-    <x-headings.h1>Heading 1</x-headings.h1>
-    <p>Some content goes here</p>
-    <x-headings.h2>Heading 2</x-headings.h2>
-    <x-headings.h3>Heading 3</x-headings.h3>
-    <p>Some information about heading 3 goes here</p>
-    <x-headings.h3>Heading 3</x-headings.h3>
-    <p>Some information about heading 3 goes here</p>
-    <x-headings.h3>Heading 3</x-headings.h3>
-    <p>Some information about heading 3 goes here</p>
-
-
-    <x-button-group class="justify-center">
-      <x-button class="btn-success">Success</x-button>
-      <x-button class="btn-info">Info</x-button>
-      <x-button class="btn-warning">Warning</x-button>
-      <x-button class="btn-error">Error</x-button>
-    </x-button-group>
+    <x-text.page-title title="About" subtitle="Find out about passworld"/>
   </x-page-container>
 </x-app-layout>
