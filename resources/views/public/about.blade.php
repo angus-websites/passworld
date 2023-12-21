6266208025bdc4cb97d3f4a488966e85cd1588d6
Angus Goody <angus.goody@gmail.com>
2023-12-21 23:18:53 +0000

    Upgrade (#27)
    * updated deps
    
    * tweaks
    
    * added Dockerfile and scripts
    
    * added workflow

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -3,5 +3,9 @@
 <x-app-layout>
   <x-page-container>
     <x-text.page-title title="About" subtitle="Find out about passworld"/>
+      <div class="my-10">
+          <p class="py-6">Passworld was created during lockdown when I was quite bored.</p>
+          <p> Angus </p>
+      </div>
   </x-page-container>
 </x-app-layout>
